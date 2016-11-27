62713e9feaa79bb40db7627894b7f36c80a22d55
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-11-27 19:29:28 +0600

    Added RegisterActivityDemo

---
+++ RegisterActivityDemo/app/src/main/java/ghose/mithun/com/registeractivitydemo/SecondActivity.java
@@ -0,0 +1,20 @@
+package ghose.mithun.com.registeractivitydemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mithun Kumer Ghose.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_layout);
+
+//        getSystemService()
+    }
+}
---
+++ RegisterActivityDemo/app/src/main/java/ghose/mithun/com/registeractivitydemo/application/AppControl.java
@@ -0,0 +1,14 @@
+package ghose.mithun.com.registeractivitydemo.application;
+
+import android.app.Application;
+
+/**
+ * Created by Mithun Kumer Ghose.
+ */
+
+public class AppControl extends Application {
+    @Override
+    public Object getSystemService(String name) {
+        return super.getSystemService(name);
+    }
+}
